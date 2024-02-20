926caf28a7d5cdc45683e2a665991026b87c00ba
Sophie <149930661+SophieEbby@users.noreply.github.com>
2024-02-20 20:33:36 +0000

    Updated 404 page and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MyNav from './components/Navbar';
-import { Contact, Custom404 Home, ProjectGallery } from './components/Pages'
+import { Contact, Custom404, Home, ProjectGallery } from './components/Pages'
 import './App.css'
 
 function App() {
--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -1,4 +1,4 @@
-export { default as Custom404 } from  './Custom404';
+export { default as Custom404 } from  './404';
 export { default as Contact } from './Contacts'
 export { default as Home } from './Home'
 export { default as ProjectGallery } from './ProjectGallery'
