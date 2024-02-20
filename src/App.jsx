8b5b086c2b129946e7d57868a1c30cac0ea5c4b2
Sophie <149930661+SophieEbby@users.noreply.github.com>
2024-02-20 20:31:37 +0000

    Added 404 page and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MyNav from './components/Navbar';
-import { Contact, Home, ProjectGallery } from './components/Pages'
+import { Contact, Custom404 Home, ProjectGallery } from './components/Pages'
 import './App.css'
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
     <Routes>
       <Route path ='/ProjectGallery' element={<ProjectGallery />} />
       <Route path ='/contact' element={<Contact />} />
+      <Route path ='/404' element={<Custom404 />} />
       <Route path ='' element={<Home />} />
     </Routes>
     </Router>
---
+++ src/components/Pages/404.jsx
@@ -0,0 +1,9 @@
+const Custom404 = () => {
+    return (
+        <>
+        <h1>404 Oops...you're lost! Please use your back button to go back!</h1>
+        </>
+    )
+}
+
+export default Custom404;
