a4940b24fc84b1fb6df98636240a3b1428ab6aec
Sophie <149930661+SophieEbby@users.noreply.github.com>
2024-02-20 20:23:58 +0000

    added gh-pages, added base to router and vite.config, adjusted scripts in package.json

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   return (
-    <Router basename={'/'}>
+    <Router basename={'/react-portfolio'}>
       <MyNav />
     <Routes>
       <Route path ='/ProjectGallery' element={<ProjectGallery />} />
