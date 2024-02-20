112e51c5533bc8d1dfa30ab3c4ad369110e38d1f
Sophie <149930661+SophieEbby@users.noreply.github.com>
2024-02-20 21:16:21 +0000

    updated index.jsx to fix the nav bar buttons

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -12,9 +12,9 @@ function MyNav() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} to='/'>Home</Nav.Link>
-                        <Nav.Link as={Link} to="/ProjectGallery">Project Gallery</Nav.Link>
-                        <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
+                        <Nav.Link href='/'>Home</Nav.Link>
+                        <Nav.Link href="/ProjectGallery">Project Gallery</Nav.Link>
+                        <Nav.Link href="/Contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
