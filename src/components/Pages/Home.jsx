4d530527fbde0ccde021ae3bd7955bda4f954b3f
Sophie <149930661+SophieEbby@users.noreply.github.com>
2024-02-21 10:44:33 +0000

    update colors

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -14,14 +14,14 @@ function Home() {
             <Row>
                 <Col sm={1}></Col>
                 <Col sm={10}>
-                    <h1>Welcome to My Portfolio</h1>
+                    <h1 className="heading1">Welcome to My Portfolio</h1>
                 </Col>
             </Row>
             <Row>
                 <Col sm={1}></Col>
                 <Col sm={10}>
                     <Image fluid src={mypic} alt="Sophie Ebsworth" className="mypic float-left" />
-                    <h2>About me</h2>
+                    <h2 className="heading2">About me</h2>
                     <p className="biotext">I am a 28 year old female, living in Southampton England. I am a qualifed SCADA Engineer, having spent 9 years developing control systems for various industries such as Railway, Airports and Water. I have a BSc (Hons) in Electrical and Electronic Engineering from the <a href="https://www.derby.ac.uk/" rel="noreferrer noopener" target="_blank">University of Derby</a>  and I gained my further education through a <a href="https://www.siemens.com/global/en.html" rel="noreferrer noopener" target="_blank">Siemens</a> apprenticeship. I am now looking to develop my skills further and become a Front End Developer. </p>
                     <Button variant="primary" href={mycv}download="SophieEbsworth_CV.pdf">Download CV</Button>
                 </Col>
