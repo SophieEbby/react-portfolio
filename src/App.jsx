4e2d905aa04af24537cbbb5b6611a82bf5cdae12
Sophie <149930661+SophieEbby@users.noreply.github.com>
2024-02-21 08:58:01 +0000

    fixing navbar buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import MyNav from './components/Navbar';
-import { Contact, Custom404, Home, ProjectGallery } from './components/Pages'
-import './App.css'
+import { Contact, Custom404, Home, ProjectGallery } from './components/Pages';
+import './App.css';
 
 function App() {
   return (
-    <Router basename={`react-portfolio`}>
+    <Router basename={`/react-portfolio`}>
       <MyNav />
-    <Routes>
-      <Route path ='/ProjectGallery' element={<ProjectGallery />} />
-      <Route path ='/contact' element={<Contact />} />
-      <Route path ='/404' element={<Custom404 />} />
-      <Route path ='' element={<Home />} />
-    </Routes>
+      <Routes>
+        <Route path="/ProjectGallery" element={<ProjectGallery />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/404" element={<Custom404 />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
+
