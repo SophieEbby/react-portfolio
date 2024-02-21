151db8e1255c12b3d2b6316fca8361870974bb68
Sophie <149930661+SophieEbby@users.noreply.github.com>
2024-02-21 10:07:39 +0000

    updated to try and fix the deployed pages again

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MyNav from './components/Navbar';
 import { Contact, Custom404, Home, ProjectGallery } from './components/Pages';
-import './App.css';
+import './App.css'
 
 function App() {
+
   return (
     <Router basename={`/react-portfolio`}>
       <MyNav />
       <Routes>
-        <Route path="/ProjectGallery" element={<ProjectGallery />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/404" element={<Custom404 />} />
-        <Route path="/" element={<Home />} />
+        <Route path='/portfolio' element={<ProjectGallery />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/404' element={<Custom404 />} />
+        <Route path='' element={<Home />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,3 @@
-
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,13 +7,13 @@ function MyNav() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Sophie Ebsworth Portfolio</Navbar.Brand>
+                <Navbar.Brand href="#">Sophie Ebsworth Portfolio</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href='/'>Home</Nav.Link>
-                        <Nav.Link href="/ProjectGallery">Project Gallery</Nav.Link>
-                        <Nav.Link href="/Contact">Contact</Nav.Link>
+                        <Nav.Link href="./">Home</Nav.Link>
+                        <Nav.Link href="./portfolio">Portfolio</Nav.Link>
+                        <Nav.Link href="./contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
